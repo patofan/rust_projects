a09c5eff37a9fd70567c59c5d221ff8d7ee2ac9f
patofan <patofan@gmail.com>
2015-06-14 11:39:42 +0800

    1.0

--- rust_stack/src/main.rs
+++ rust_stack/src/main.rs
@@ -1,3 +1,42 @@
+pub struct Stack<E> {
+	vec : Vec<E> ,
+}
+
+impl <E> Stack<E> {
+	fn new() -> Stack<E> {
+		Stack{ vec : vec![]  }
+	}
+	
+	fn push(&mut self ,  e : E ) {
+	   self.vec.push( e );	
+	}
+	
+	fn pop(&mut self ) -> Option<E> {
+		return self.vec.pop();
+	}
+	
+    fn peek(&self) -> Option<&E> {
+        return self.vec.last();
+    }
+    
+    fn is_empty(&self) -> bool {
+    	self.vec.is_empty()
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+	let mut s = Stack::new();
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	while ! s.is_empty() {
+		let e = s.pop();
+		match e {
+			Some( x ) => println!( "{}" ,  x ) ,
+			_ => {}
+		}
+	}
+	
+	
 }
